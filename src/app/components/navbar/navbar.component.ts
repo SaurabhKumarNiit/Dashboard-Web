6620a219fb86daad17c5066c3ffc2e94713f1222
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2024-03-04 13:10:56 +0530

    updated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -130,16 +130,16 @@ export class NavbarComponent implements OnInit {
     };
 
     getTitle(){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      if(titlee.charAt(0) === '#'){
-          titlee = titlee.slice( 1 );
-      }
+    //   var titlee = this.location.prepareExternalUrl(this.location.path());
+    //   if(titlee.charAt(0) === '#'){
+    //       titlee = titlee.slice( 1 );
+    //   }
 
-      for(var item = 0; item < this.listTitles.length; item++){
-          if(this.listTitles[item].path === titlee){
-              return this.listTitles[item].title;
-          }
-      }
-      return 'Dashboard';
+    //   for(var item = 0; item < this.listTitles.length; item++){
+    //       if(this.listTitles[item].path === titlee){
+    //           return this.listTitles[item].title;
+    //       }
+    //   }
+      return 'CCL Dashboard';
     }
 }
